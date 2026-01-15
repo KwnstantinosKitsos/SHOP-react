e5c52305857968c104c08ec1a25094e44106027c
Kwstas <ntinoskitsios1@gmail.com>
2026-01-15 18:00:38 +0200

    Complete PaymentSummary UI

--- src/components/PaymentSummary/PaymentSummary.jsx
+++ src/components/PaymentSummary/PaymentSummary.jsx
@@ -1,7 +1,49 @@
+import './PaymentSummary.css';
 export default function PaymentSummary() {
   return (
     <>
-      <p>Summary Payment</p>
+      <div className="payment-container">
+        <h2 className="payment-text">Payment Summary</h2>
+        {/* Payment Details */}
+        <div className="payment-info">
+          {/* Items */}
+          <div className="payment-info-items space">
+            <div className="payment-item-text">
+              Items<span className="payment-item-quantity"> (1)</span>:
+            </div>
+            <div className="payment-item-price">
+              <span className="">$799.99</span>
+            </div>
+          </div>
+          {/* Shipping */}
+          <div className="payment-info-shipping space">
+            <div className="payment-info-shipping-text">
+              Shipping & handling:
+            </div>
+            <div className="payment-info-shipping-price">$0.00</div>
+          </div>
+          <hr className=" horizontal" />
+          {/* Before TAX */}
+          <div className="payment-info-b-tax space">
+            <div className="payment-info-b-tax-text">Total before tax:</div>
+            <div className="payment-info-b-tax-price">$799.97</div>
+          </div>
+          {/* After TAX */}
+          <div className="payment-info-a-tax space">
+            <div className="payment-info-a-tax-text">Estimated tax (10%):</div>
+            <div className="payment-info-a-tax-price">$79.99</div>
+          </div>
+          <hr className=" horizontal" />
+          {/* Total */}
+          <div className="payment-info-total space">
+            <div className="payment-info-total-text">Order total:</div>
+            <div className="payment-info-total-price">$880.00</div>
+          </div>
+        </div>
+        <div className="payment-container-btn">
+          <button className="payment-btn">Place your order</button>
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -4,7 +4,9 @@ import './CartPage.css';
 export default function CartPage() {
   return (
     <div className="cart-page">
-      <h1 className="cart-title">Checkout (1)</h1>
+      <h1 className="cart-title">
+        Checkout <span className="art-title-quantity">(1)</span>
+      </h1>
       <h2 className="cart-preview-text">Review your order</h2>
       <div className="cart-grid-layout">
         <main className="cart-left">
