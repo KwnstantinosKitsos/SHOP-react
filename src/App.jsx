64cdb000974538f7a07f599b916deadc3e8f1d76
Kwstas <ntinoskitsios1@gmail.com>
2026-01-15 19:24:55 +0200

    Display UI message when cart is empty

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ShopPage from './pages/ShopPage/ShopPage';
 import CartPage from './pages/CartPage/CartPage';
+import OrdersPage from './pages/OrdersPage/OrdersPage';
+
 function App() {
   return (
     <>
@@ -12,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ShopPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,7 +1,21 @@
 import PaymentSummary from '../../components/PaymentSummary/PaymentSummary';
 import PreviewOrder from '../../components/PreviewOrder/PreviewOrder';
 import './CartPage.css';
+import { Link } from 'react-router-dom';
 export default function CartPage() {
+  const itemList = [1, 2, 3, 4, 5, 6];
+
+  if (itemList.length === 0) {
+    return (
+      <>
+        <div className="cart-empty">
+          <h1>Your cart is empty</h1>
+          <p>Add some products to get started!</p>
+          <Link to="/">Continue shopping</Link>
+        </div>
+      </>
+    );
+  }
   return (
     <div className="cart-page">
       <h1 className="cart-title">
@@ -10,7 +24,14 @@ export default function CartPage() {
       <h2 className="cart-preview-text">Review your order</h2>
       <div className="cart-grid-layout">
         <main className="cart-left">
-          <PreviewOrder />
+          <div className="cart-items">
+            <PreviewOrder />
+            <PreviewOrder />
+            <PreviewOrder />
+            <PreviewOrder />
+            <PreviewOrder />
+            <PreviewOrder />
+          </div>
         </main>
         <aside className="cart-right">
           <PaymentSummary />
