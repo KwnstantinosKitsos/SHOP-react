f6c45af3898305743282aa372d664f9b2f60d128
Kwstas <ntinoskitsios1@gmail.com>
2026-01-20 13:10:27 +0200

    Use useLocation hook to display search only on ShopPage

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,9 +2,13 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 import { useSearch } from '../../context/SearchContext';
 import { useCart } from '../../context/CartContext';
+import { useLocation } from 'react-router-dom';
+
 export default function Navbar() {
   const { searchItems, setSearchItems } = useSearch();
   const { cart } = useCart();
+  //Current Location
+  const location = useLocation();
 
   return (
     <>
@@ -21,20 +25,22 @@ export default function Navbar() {
             </div>
           </div>
           {/* Search Bar */}
-          <div className="con2">
-            <div className="search-container">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
-                <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" />
-              </svg>
-              <input
-                type="text"
-                className="input"
-                placeholder="Search..."
-                value={searchItems}
-                onChange={(event) => setSearchItems(event.target.value)}
-              />
+          {location.pathname === '/' && (
+            <div className="con2">
+              <div className="search-container">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                  <path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z" />
+                </svg>
+                <input
+                  type="text"
+                  className="input"
+                  placeholder="Search..."
+                  value={searchItems}
+                  onChange={(event) => setSearchItems(event.target.value)}
+                />
+              </div>
             </div>
-          </div>
+          )}
 
           {/* Action Buttons */}
           <div className="con3">
